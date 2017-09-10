df13188a9c9be6511052805139036bd500619b55
sushicai@163.com <sushicai@163.com>
2017-09-10 22:28:29 +0800

    first commit

---
+++ yii2test/views/site/entry-confirm.php
@@ -0,0 +1,9 @@
+<?php
+use yii\helpers\Html;
+?>
+<p>你输入的信息如下:</p>
+
+<ul>
+    <li><label>Name</label>: <?= Html::encode($model->name) ?></li>
+    <li><label>Email</label>: <?= Html::encode($model->email) ?></li>
+</ul>
